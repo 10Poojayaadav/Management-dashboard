33cec011f2ccae905e9a16de2ec7021b2d617bcd
Pooja Yadav <pooja@singsys.com>
2025-01-23 16:59:56 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,13 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import AppRouter from "./router";
 import Header from "./component/Header";
 import SideBar from "./component/SideBar";
-import "@fontsource/roboto"; // Default weight 400
+import "@fontsource/roboto"; 
 import "@fontsource/roboto/700.css";
 import Login from "./pages/Login";
 
 
 export default function App() {
   const location = useLocation();
-
-  // Check if the current path is the login page
   const isLoginPage = location.pathname === "/login";
   return (
     <>
@@ -21,10 +19,7 @@ export default function App() {
       </Routes>
     ) : (
     <div className="flex">
-      {/* Sidebar */}
       <SideBar />
-
-      {/* Main Content */}
       <div className="flex-1 bg-gray-100">
         <header className="p-1 bg-white shadow">
           <h1 className="text-xl font-semibold"><Header/></h1>
